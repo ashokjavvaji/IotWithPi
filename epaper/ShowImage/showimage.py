da325bb93332fae446bfd043d60bd2ae99dc2666
Ashok <javvaji.ashok@gmail.com>
2018-10-05 23:57:55 +0530

    Update showimage.py

--- epaper/ShowImage/showimage.py
+++ epaper/ShowImage/showimage.py
@@ -12,17 +12,8 @@ def main():
 
     # For simplicity, the arguments are explicit numerical coordinates
     #image = Image.new('1', (EPD_WIDTH, EPD_HEIGHT), 1)    # 1: clear the frame
-    image = Image.open('image3.bmp')
-    img = Image.open('hearts1.bmp')
-    img_w, img_h = img.size
-    bg_w, bg_h = image.size
-    #offset = ((bg_w - img_w) // 2, (bg_h - img_h) // 2)
-    offset = (0,0)
-    image.paste(img, offset)
+    image = Image.open('tajmahal.bmp')
     epd.display_frame(epd.get_frame_buffer(image))
-
 
 if __name__ == '__main__':
     main()
-
-
